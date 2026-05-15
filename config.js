0a72dbcf50072ba2c9ee59d8bd7c41bd9dac84c7
mnsalazar01-develop <mnsalazar01@gmail.com>
2026-05-15 16:31:08 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+// =========================================================================
+// ARCHIVO CENTRAL DE CONFIGURACIÓN POS - MODIFÍCALO SOLO UNA VEZ DE POR VIDA
+// =========================================================================
+const CONFIG = {
+    SUPABASE_URL = "https://pmhusucbjptpsahrxird.supabase.co"
+    SUPABASE_KEY = "sb_publishable_KX7Ap3Led8LZbIeQwHEhxQ_QNIWtru_"
+};
+
+
+
